01015106b1d0d5ece7b1ec2be5dde8a08f378bd6
Marco Klein <marco.klein@trustedshops.de>
2021-09-06 18:27:52 +0200

    Add navigation memory for home component.

--- website/src/views/home/home.component.ts
+++ website/src/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { NavigationMemoryService } from '../../services/navigation-memory.service';
 
 @Component({
   selector: 'home',
@@ -7,4 +8,24 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
+  //#region Properties
+  private _wasOpened: boolean;
+  public get wasOpened(): boolean {
+    return this._wasOpened;
+  }
+  public set wasOpened(v: boolean) {
+    this._wasOpened = v;
+  }
+
+  //#endregion
+
+  //#region Ctor
+  public constructor(private readonly _navigationMemoryService: NavigationMemoryService) { }
+  //#endregion
+
+  //#region Public Methods
+  public ngOnInit() {
+    this._wasOpened = this._navigationMemoryService.wasOpened('home');
+  }
+  //#endregion
 }
