22629d66f6bb927a107a9382d41926985cda127d
Marco Klein <marco.klein@trustedshops.de>
2021-09-08 00:28:16 +0200

    Updating descriptions

--- website/src/configuration/stations.ts
+++ website/src/configuration/stations.ts
@@ -4,7 +4,7 @@ export const stations: CareerStation[] = [{
   companyName: 'EA Games/Bright Future GmbH',
   job: 'Trainee Developer/Quality Assurance',
   atPath: 14.5 / 100,
-  description: 'During my time in school we were advised to do a training on a specific profession we prefer to learn in the future. My deepest desire was to become a professional game developer. Due to the possibilities and connections I had during the year 2006, I was given the chance to do a training inside the offices of a development studio of EA Games. Bright Future was the company developing the `Fußball Manager` title of each year.\n\nWhile pro-actively extending my trainee-period of two weeks to four in total, I got to know certain tasks of software development in general, but also insights into how computer games are created. This was including the regular testing, preparing press media, developing the game itself and seeing how graphical elements evolve.',
+  description: 'During my time in school we were advised to do a training on a specific profession we prefer to learn in the future. My deepest desire was to become a professional game developer. Due to the possibilities and connections I had during the year 2006, I was given the chance to do a training inside the offices of a development studio of EA Games. Bright Future was the company developing the `Fußball Manager` title of each year.\n\nWhile pro-actively extending my trainee-period of two weeks to four in total, I got to know certain tasks of software development in general, but also insights into how video games are created. This was including the regular testing, preparing press media, developing the game itself and seeing how graphical elements evolve.',
   start: {
     month: 6,
     year: 2006,
@@ -42,7 +42,17 @@ export const stations: CareerStation[] = [{
 }, {
   companyName: 'Serviceware SE',
   job: 'Apprenticeship: Computer Science Expert',
-  description: 'After finishing my graduation at a german Gymnasium, I applied for studying in an integrated degree program. That means, I was doing the studies and my apprenticeship at the same time. This is why it is also called `Dual Degree` (literally translated) in German.\n\nI joined the Serviceware SE, formerly called `PMCS.helpLine Software Group` in the location in Hürth.\n\nStarting off from the quality assurance department, I got tought how testing works from A-Z. Because of a missing automation in reportings, the QA team was also responsible for the daily reports of bugs and fixes - There I had my first projects creating an automation for it. \n\nAfter that, I was planning my first IT-System for Software-Development: It was a test lab, a multi-server environment connected to Microsoft Test Manager.\n\n Then I switched over to the development department: Here I developed on applications like Rich-Clients, WebApps, and Server-Side environments. Since it was company relying on Microsoft products, we 90% used C# as Coding language.',
+  description: `After finishing my graduation at a german Gymnasium, I applied for studying in an integrated degree program.
+  That means, I was doing the studies and my apprenticeship at the same time. This is why it is also called \`Dual Degree\` (literally translated) in German.
+
+  I joined the Serviceware SE, formerly called \`PMCS.helpLine Software Group\` in the location in Hürth.
+  Starting off from the quality assurance department, I got tought how testing works from A-Z. Because of a missing automation in reportings, the QA team was also responsible for the daily reports of bugs and fixes - There I had my first projects creating an automation for it.
+  Furthermore, I picked up topics like improving continuous integration pipelines with Microsoft Team Foundation Server.
+  For the QA Team, I also created Test Plans and learned how tests are organized professionally.
+
+  After that, I was planning my first IT-System for Software-Development: It was a test lab, a multi-server environment connected to Microsoft Test Manager.
+
+  Then I switched over to the development department: Here I was a part of the development of applications like Rich-Clients, WebApps, and Server-Side environments. Since it was company relying on Microsoft products, we 90% used C# as Coding language.`,
   atPath: 35.3 / 100,
   start: {
     month: 7,
@@ -56,6 +66,11 @@ export const stations: CareerStation[] = [{
   companyName: 'Serviceware SE',
   job: 'Work Student: Software Engineer',
   atPath: 49 / 100,
+  description: `My apprenticeship was shortened by 6 months and sucessfully finished, so I continued to work in the company as a working student.
+  There, I continued to work as a software engineer in the Research and Development division.
+
+  In that time I was responsible for developing a communication interface between two of our systems developed in-house.
+  In the product developed in Hürth, I managed the contracts and APIs for migrating data from/to the systems together with the team that was working on the second product.`,
   start: {
     month: 2,
     year: 2011,
@@ -67,6 +82,12 @@ export const stations: CareerStation[] = [{
 }, {
   companyName: 'EUFH Bruehl',
   job: 'Bachelor Degree: Business Information Systems',
+  description: `In 2014, I finally got my bachelor degree in information systems and completed my studies of three years. In that time, I learned topics from the following areas:
+
+    - Business economics
+    - Information Systems (Software development, IT Infrastructure, etc.)
+    - Soft Skills
+    - Languages (English & Dutch)`,
   atPath: 57 / 100,
   school: true,
   start: {
@@ -81,6 +102,15 @@ export const stations: CareerStation[] = [{
   companyName: 'Serviceware SE',
   job: 'Full-Stack Software Engineer',
   atPath: 65.3 / 100,
+  description: `At the end of my integrated degree program I signed a contract to continue working for Serviceware SE.
+  My first business project was exciting: I was set up to travel back and forth to the dutch site of the company, starting as a full stack developer in the dutch team. There, I was developing web frontends with angular.js and REST APIs written in ASP.NET.
+  Furthermore I brought in my expertise on how to set up modern continuous integration pipelines with Microsoft Team Foundation Server.
+
+  After 1.5 years, I turned back to the site in Hürth, I joined the UX Team, which was mainly responsible for developing the client products of the system. There I was a part developing
+
+    - a WPF Rich Client Application
+    - a migration of a consumer portal to angular.js and a modern architecture
+    - a brand-new mobile app, written in angular.js and ported with Apache Cordova`,
   start: {
     month: 10,
     year: 2014,
@@ -93,6 +123,13 @@ export const stations: CareerStation[] = [{
   companyName: 'Trusted Shops GmbH',
   job: 'Senior Full-Stack Software Engineer',
   atPath: 73.5 / 100,
+  description: `At Trusted Shops, I officially started as a frontend developer only. I started as a part of the Consumer UX Team there, being responsible for the consumer web frontend. I picked up Angular 2 there, learning and expanding my knowledge up to the latest Angular version til today.
+  After working on the consumer platform, I continued to work on a website relaunch, based on Wordpress and Angular.
+  As this was finished, I continued to rebuild another new satellite of the Website with Angular - but this time, in combination with Angular Universal and SEO optimizations.
+
+  Since I also wanted to bring in my infrastructure and backend skills, I implemented a modular and pluggable test environment created with docker.
+
+  Today, I am responsible for building a pluggable micro-frontend platform and reusable libraries together with my current team.`,
   start: {
     month: 2,
     year: 2018,
